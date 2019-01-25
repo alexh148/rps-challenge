048c72ef1926391e0c6bacc4b6d5087784001732
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-25 10:59:15 +0000

    First user story completed with basic UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,22 @@ require 'sinatra/base'
 class RPS < Sinatra::Base
 
   set :root, File.join(File.dirname(__FILE__), '..')
-  set :views, Proc.new { File.join(root, "views") } 
+  set :views, Proc.new { File.join(root, "views") }
+
+  enable :sessions
 
   get '/' do
     erb(:index)
+  end
+
+  post '/user_name' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -1,3 +1,12 @@
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
+
+feature 'registers the users name' do
+  scenario 'when the page is first loaded' do
+    visit '/'
+    fill_in 'name', with: 'Al'
+    click_button 'Go!'
+    expect(page).to have_content('Welcome, Al!')
+  end
+end
