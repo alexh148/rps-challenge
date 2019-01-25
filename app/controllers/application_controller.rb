6953a0e1a55d99d472fe40f7c7b06218a0b0e95f
Alex Hutchings <alexhutchings4@gmail.com>
2019-01-25 15:03:59 +0000

    Finished functionality so game now works and declares the winner/loser/draw. Next step: Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require_relative '../models/player'
+require_relative '../models/game'
 
 class RPS < Sinatra::Base
 
@@ -29,12 +30,26 @@ class RPS < Sinatra::Base
     @computer = $computer
     @game = Game.new
     redirect '/winner' if @game.winner(@player, @computer) == @player
+    redirect '/loser' if @game.winner(@player, @computer) == @computer
+    redirect '/draw' if @game.winner(@player, @computer) == 'draw'
   end
 
   get '/winner' do
     @player = $player
     @computer = $computer
     erb(:winner)
+  end
+
+  get '/loser' do
+    @player = $player
+    @computer = $computer
+    erb(:loser)
+  end
+
+  get '/draw' do
+    @player = $player
+    @computer = $computer
+    erb(:draw)
   end
 
   run! if app_file == $0
